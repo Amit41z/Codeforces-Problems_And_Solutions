129c2b71d9d9e242e62ec052bb1cd7d198761326
Amit Kumar Ghosh <amitghosh41001@gmail.com>
2023-09-04 18:47:58 +0530

    Added Successfully

---
+++ 266A Stones on the table/Solution.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    int i, n, c = 0;
+    cin >> n;
+    string s;
+    cin >> s;
+    for (i = 0; i < n; i++)
+    {
+        if (s[i] == s[i + 1])
+        {
+            c++;
+        }
+    }
+    cout << c;
+}
