6288436907f79223361e122639bbdfa6cb05a31a
Amit Kumar Ghosh <amitghosh41001@gmail.com>
2023-09-12 20:44:19 +0530

    Added Successfully

---
+++ 50A Domino piling/Solution.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main()
+{
+    double m, n;
+    long long f;
+    cin >> m >> n;
+    f = floor((m * n) / 2);
+    cout << f;
+    return 0;
+}
